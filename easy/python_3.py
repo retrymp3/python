bffa23bbe1d8c5559f33d7892ce53b37e3b5a760
retrymp3 <65720815+retrymp3@users.noreply.github.com>
2020-12-28 17:16:51 +0530

    Update python_3.py

--- easy/python_3.py
+++ easy/python_3.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 n=int(input("Enter the number of items to be entered: "))
 i=(input("Provide the space seperated items: "))
-queue=i.split()
+queue=i.split() #Splits the string into a list
 stack=[]
 
 print("\nRetriving items from the queue\n")
-for q in range(n):
+for q in range(n): #First in first out
 	p=queue.pop(0)
 	stack.append(p)
 	print(p)
 
 print("\nRetriving items from the stack\n")
 for s in range(n):
-	print(stack.pop())
+	print(stack.pop()) #Last in last out
