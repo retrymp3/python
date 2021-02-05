696246b4ed38f8e406ed457724a44bd07d18f2a2
retrymp3 <65720815+retrymp3@users.noreply.github.com>
2021-02-05 20:25:39 +0530

    Update s2_server.py

--- medium/s2_server.py
+++ medium/s2_server.py
@@ -6,33 +6,56 @@ import hmac
 from Crypto.Cipher import AES
 import time
 import socket
+import os
+from Crypto import Random
 
-key = b'strawhatpirateee' # Byte format key
-iv = b'piratekingstraww' # Byte format initialization vector
+key = "strawhatpirateee"
 
-def pad(s): # For adding extra spaces so the message is supplied as 16-byte blocks
+# For adding extra spaces at the endso the message becomes a multiple of 16 in bytes.
+# AES needs 16 byte blocks
+def pad(s):
     block_size = 16
     remainder = len(s) % block_size
     padding_needed = block_size - remainder
     return s + padding_needed * ' '
 
+# Creating a private key by adding the salt to the key.
+# IV is used so that it can prevent duplicate characters in the cipher text.
+# Salt is generated randomly each time the script is run.
+# Scrypt is used for making a more secure private key.
+# N - Cost factor, r - Block size, p - parallelization factor
+def encrypt(msg, key):
+	salt = os.urandom(AES.block_size) # Generating a random salt.
+	iv = Random.new().read(AES.block_size) # Generating a random initialization vector.
+	private_key = hashlib.scrypt(key.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32) # Creating a private key by adding salt.
+	aes = AES.new(private_key, AES.MODE_CBC, iv) # Configuring the encryption mode to CBC(Cipher Blocker Chaining)
+	padded_text = pad(msg)
+	aes_text = aes.encrypt(padded_text)
+	return (aes_text,salt,iv)
+
+# AF_INET specifies the ipv4 address family.
+# SOCK_STREAM specifies the TCP socket
 
 node = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port_and_ip = ('127.0.0.1', 4444)
-node.bind(port_and_ip)   
-node.listen(1) 
+port_and_ip = ('127.0.0.1', int(input("Port: ")))
+node.bind(port_and_ip)   # Assigns an ip and port number to the socket instance.
+node.listen(1) # Limits the number of connections
 cnn, addr = node.accept() 
 
 print ("Connection from: " + str(addr) )
 
 msg = input("Message: ")
-AES = AES.new(key, AES.MODE_CBC, iv)
-pad = pad(msg)
-encrypted = AES.encrypt(pad) # Encrypting the padded message.
-msg_unicode = msg.encode('utf-8')
-hsh_hmac = hmac.new(key,msg_unicode,hashlib.sha512)
-cnn.send(encrypted)
+aes_text,salt,iv = encrypt(msg,key)
+msg_encode = msg.encode() # Hashing requires unicode-objects to be encoded. Utf-8 is used for character encoding.
+hsh_hmac = hmac.new(key.encode(),msg_encode,hashlib.sha512)
+
+cnn.send(iv)
 time.sleep(1)
-cnn.send(hsh_hmac.hexdigest().encode('utf-8'))
+cnn.send(salt)
+time.sleep(1)
+cnn.send(aes_text)
+time.sleep(1)
+cnn.send(hsh_hmac.hexdigest().encode())
+time.sleep(1)
 
 node.close()
